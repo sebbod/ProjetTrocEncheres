5ec6c2216bed4316690ce541a5eda1a7d0687121
sebbod <bod456@gmail.com>
2021-11-22 11:15:07 +0100

    SQL informations

---
+++ WebContent/META-INF/SQL_History/insert_bd_bids4MSSQL_v1.sql
@@ -0,0 +1,22 @@
+INSERT INTO [dbo].[USERS] ([pseudo],[name],[firstName],[email],[telephone],[street],[zipCode],[town],[pwd],[credit],[isAdmin])
+VALUES ('root','root','root','root@root.fr','','','','','root',0,1);
+INSERT INTO [dbo].[USERS] ([pseudo],[name],[firstName],[email],[telephone],[street],[zipCode],[town],[pwd],[credit],[isAdmin])
+VALUES ('toto','toto','toto','toto@toto.fr','','','','','toto',0,0);
+
+
+
+
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison-Ameublement');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison-Electroménager');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison-Briolage jardinage');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Mode-Vêtements');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Mode-Chaussures');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Mode-Accessoires');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Multimédia-Informatique');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Multimédia-Jeux vidéo');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Multimédia');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs-Sport');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs-Musique');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs-Jeux');
