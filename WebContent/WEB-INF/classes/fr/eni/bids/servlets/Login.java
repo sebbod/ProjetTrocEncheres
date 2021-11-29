f8393bae195e91de1c145573a801a194eecca6f4
sebbod <bod456@gmail.com>
2021-11-29 13:25:26 +0100

    correctif si on cohe pas remenber me

--- WebContent/WEB-INF/classes/fr/eni/bids/servlets/Login.java
+++ WebContent/WEB-INF/classes/fr/eni/bids/servlets/Login.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +34,18 @@ public class Login extends HttpServlet {
 	 * Show login page
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie[] cookies = request.getCookies();
+
+		// If remember me cookie is find, complete username
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				System.out.println(cookie.getName());
+				if (cookie.getName().equals("username")) {
+					System.out.println(cookie.getValue());
+					request.setAttribute("username", cookie.getValue());
+				}
+			}
+		}
 		RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/Login.jsp");
 		rd.forward(request, response);
 	}
@@ -69,6 +82,17 @@ public class Login extends HttpServlet {
 
 		// If valid username/password, create session and redirect to homepage
 		if (id != 0) {
+
+			// If "remember me" is checked, create cookie with username
+			if (request.getParameter("remember") != null) {
+				if (request.getParameter("remember").equals("checked")) {
+					Cookie cookie = new Cookie("username", username);
+					// Expiration in 1 month
+					cookie.setMaxAge(2628000);
+					response.addCookie(cookie);
+				}
+			}
+
 			HttpSession session = request.getSession();
 			session.setAttribute("connectedUserId", id);
 
