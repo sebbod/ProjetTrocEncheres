7f0dde2956a44934ddd9b32093643a1d0a97ed6b
sebbod <bod456@gmail.com>
2021-11-30 07:57:56 +0100

    update test data

--- WebContent/META-INF/SQL_History/insert_bd_bids4MSSQL_v1.sql
+++ WebContent/META-INF/SQL_History/insert_bd_bids4MSSQL_v1.sql
@@ -1,22 +1,45 @@
 INSERT INTO [dbo].[USERS] ([pseudo],[name],[firstName],[email],[telephone],[street],[zipCode],[town],[pwd],[credit],[isAdmin])
-VALUES ('root','root','root','root@root.fr','','','','','root',0,1);
+VALUES ('root','root','root','root@root.fr','','root','root','root','prhRGjOQDBM=',0,1);
 INSERT INTO [dbo].[USERS] ([pseudo],[name],[firstName],[email],[telephone],[street],[zipCode],[town],[pwd],[credit],[isAdmin])
-VALUES ('toto','toto','toto','toto@toto.fr','','','','','toto',0,0);
+VALUES ('toto','toto','toto','toto@toto.fr','','toto','toto','toto','adXEDYhkhQU=',0,0);
+
+
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Informatique');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Ameublement');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Vêtemtent');
+INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Sport&Loisirs');
+
+
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6200', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'créer');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6300', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'annuler');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6400', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6500', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'terminées');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6600', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'retiré');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6700', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6800', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 6900', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 7000', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 7100', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 7200', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 7300', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 7400', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 7500', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
+INSERT INTO [dbo].[BID_ITEMS]([name],[description],[dateStart],[dateEnd],[priceSeller],[priceBuyer],[userIdSeller],[cateId],[status])
+VALUES ('DELL inspiron 7600', 'ordinateur 4Go de RAM, disque SSD 120Go, écran 17 pouce, windows installé', getdate(), getdate()+30, 250,null,1,1,'en cours');
 
 
 
-
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison-Ameublement');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison-Electroménager');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Maison-Briolage jardinage');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Mode-Vêtements');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Mode-Chaussures');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Mode-Accessoires');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Multimédia-Informatique');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Multimédia-Jeux vidéo');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Multimédia');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs-Sport');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs-Musique');
-INSERT INTO [dbo].[BID_ITEM_CATEGORIES] ([libelle]) VALUES ('Loisirs-Jeux');
